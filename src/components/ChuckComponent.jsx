038448a65212ed9e12e6c8c63fed8534e25fe9c5
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-10-15 00:25:24 -0300

    add material ui

--- src/components/ChuckComponent.jsx
+++ src/components/ChuckComponent.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import getRandomChuck from '../services/axiosService'
+import Button from '@mui/material/Button'
 
 export const ChuckComponent = () => {
     const [chuck, setChuck] = useState('')
+    const [positive, setPositive] = useState(0)
+    const [negative, setNegative] = useState(0)
     
     function getChuck(){
         getRandomChuck()
@@ -16,10 +19,15 @@ export const ChuckComponent = () => {
     }
   
   return (
-    <div>
+    <div style={{ 'textAlign': 'center'}}>
         <h2>This is a random generator Chuck:</h2>
-        <button onClick={getChuck}>Click Here!</button>
+        <Button variant='contained'  onClick={getChuck}>Click Here!</Button>
         {<p>{chuck}</p>}
+
+        <Button variant='contained' color="success" onClick={() => {setPositive(positive+1)}}>POSITIVE</Button>
+        <Button variant='contained' color="error" onClick={() => {setNegative(negative+1)}}>NEGATIVE</Button>
+        <h1>LIKE: {positive}</h1>
+        <h1>DISLIKE: {negative}</h1>
     </div>
   )
 }
